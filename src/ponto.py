7498751d288d664bcf88129a51de60b81b45aacf
Philippe Menge <philippemengealima@gmail.com>
2024-05-08 14:24:30 -0300

    Refatoração da classe Ponto

--- src/ponto.py
+++ src/ponto.py
@@ -9,22 +9,22 @@ class Ponto:
         self.y = y
         self.z = z
 
-    def __sub__(self, other: Self | Vetor) -> Vetor:
-        return Vetor(self.x - other.x, self.y - other.y, self.z - other.z)
+    def __sub__(self, p2: Self | Vetor) -> Vetor:
+        return Vetor(self.x - p2.x, self.y - p2.y, self.z - p2.z)
 
-    def __add__(self, other: Self | Vetor) -> Self:
-        return Ponto(self.x + other.x, self.y + other.y, self.z + other.z)
+    def __add__(self, p2: Self | Vetor) -> Self:
+        return Ponto(self.x + p2.x, self.y + p2.y, self.z + p2.z)
 
-    def __eq__(self, other: Any) -> bool:
-        if not isinstance(other, Ponto):
+    def __eq__(self, Outro: Any) -> bool:
+        if not isinstance(Outro, Ponto):
             return False
-        return self.x == other.x and self.y == other.y and self.z == other.z
+        return self.x == Outro.x and self.y == Outro.y and self.z == Outro.z
 
-    def __mul__(self, other: float) -> Self:
-        return Ponto(self.x * other, self.y * other, self.z * other)
+    def __mul__(self, n: float) -> Self:
+        return Ponto(self.x * n, self.y * n, self.z * n)
 
-    def __rmul__(self, other: float) -> Self:
-        return self.__mul__(other)
+    def __rmul__(self, n: float) -> Self:
+        return self.__mul__(n)
 
     def __neg__(self) -> Self:
         return Ponto(-self.x, -self.y, -self.z)
