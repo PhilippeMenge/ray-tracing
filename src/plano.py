f6cf932e111961725efe6532eb376e27eed705cb
Walmir Bispo <wbsj@cin.ufpe.br>
2024-05-15 13:26:20 -0300

    Ajustando classe Plano

--- src/plano.py
+++ src/plano.py
@@ -12,12 +12,12 @@ class Plano:
     def get_normal(self) -> Vetor:
         return self.normal
 
-    def get_intersecao(self, ray: Ray) -> bool:
+    def get_intersecao(self, ray: Ray) -> float | None:
         if abs(ray.direcao.produto_escalar(self.normal)) == 0:
-            return False
+            return None
 
         distance = self.normal.produto_escalar(self.ponto - ray.origem) / ray.direcao.produto_escalar(self.normal)
         if distance > 0:
-            return True
+            return distance
 
-        return False
+        return None
