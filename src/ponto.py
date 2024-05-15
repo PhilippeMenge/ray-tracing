aebe9553cb7577d88b773d08f67fc43980d7c9f4
Walmir Bispo <wbsj@cin.ufpe.br>
2024-05-15 12:39:05 -0300

    Deletando métodos que acabaram não sendo usados

--- src/ponto.py
+++ src/ponto.py
@@ -14,20 +14,3 @@ class Ponto:
 
     def __add__(self, p2: Self | Vetor) -> Self:
         return Ponto(self.x + p2.x, self.y + p2.y, self.z + p2.z)
-
-    def __eq__(self, Outro: Any) -> bool:
-        if not isinstance(Outro, Ponto):
-            return False
-        return self.x == Outro.x and self.y == Outro.y and self.z == Outro.z
-
-    def __mul__(self, n: float) -> Self:
-        return Ponto(self.x * n, self.y * n, self.z * n)
-
-    def __rmul__(self, n: float) -> Self:
-        return self.__mul__(n)
-
-    def __neg__(self) -> Self:
-        return Ponto(-self.x, -self.y, -self.z)
-        
-    def __repr__(self) -> str:
-        return f"Ponto({self.x}, {self.y}, {self.z})"
