f38ea6e7bf2faed631a931c2b2a7924e325a49a3
Walmir Bispo <wbsj@cin.ufpe.br>
2024-05-15 12:53:04 -0300

    Deletando operações que acabaram não sendo usadas

--- src/vetor.py
+++ src/vetor.py
@@ -7,15 +7,6 @@ class Vetor:
         self.y = y
         self.z = z
 
-    def __neg__(self) -> Self:
-        return Vetor(-self.x, -self.y, -self.z)
-    
-    def __eq__(self, v2: Any) -> bool:
-        if not isinstance(v2, Vetor):
-            return False
-
-        return self.x == v2.x and self.y == v2.y and self.z == v2.z
-
     def __add__(self, v2: Self) -> Self:
         return self.__class__(self.x + v2.x, self.y + v2.y, self.z + v2.z)
 
@@ -28,9 +19,6 @@ class Vetor:
         else:
             return self.__class__(self.x * v2, self.y * v2, self.z * v2)
 
-    def __rmul__(self, v2: float) -> Self:
-        return self.__mul__(v2)
-
     def __truediv__(self, v2: Any) -> Self:
         if isinstance(v2, Vetor):
             return self.__class__(self.x / v2.x, self.y / v2.y, self.z / v2.z)
@@ -41,20 +29,13 @@ class Vetor:
     def __abs__(self) -> float:
         return math.sqrt(self.x**2 + self.y**2 + self.z**2)
 
-    def norma(self) -> float:
-        """Retorna a norma do vetor"""
-        return abs(self)
-
     def normalizado(self) -> Self:
         """Retorna o vetor normalizado"""
         return self / abs(self)
 
-    def __potencia__(self, expoente: int) -> Self:
-        return self.__class__(self.x**expoente, self.y**expoente, self.z**expoente)
-
     def produto_escalar(self, v2: Self) -> float:
         return self.x * v2.x + self.y * v2.y + self.z * v2.z
-    
+
     def produto_vetorial(self, v2: Self) -> Self:
         return self.__class__(self.y * v2.z - self.z * v2.y,
                               self.z * v2.x - self.x * v2.z,
