b3b0074107368447d422c7ff3756d24f840ae954
Walmir Bispo <wbsj@cin.ufpe.br>
2024-05-17 10:40:44 -0300

    Reescrevendo de forma mais legivel a funcao get_intersecao

--- src/plano.py
+++ src/plano.py
@@ -13,11 +13,14 @@ class Plano:
         return self.normal
 
     def get_intersecao(self, ray: Ray) -> float | None:
-        if abs(ray.direcao.produto_escalar(self.normal)) == 0:
+        denominador = ray.direcao.produto_escalar(self.normal)
+        numerador = self.normal.produto_escalar(self.ponto - ray.origem)
+
+        if denominador == 0:
             return None
 
-        distance = self.normal.produto_escalar(self.ponto - ray.origem) / ray.direcao.produto_escalar(self.normal)
-        if distance > 0:
+        distance = numerador / denominador
+        if distance >= 0:
             return distance
-
-        return None
+        else:
+            return None
