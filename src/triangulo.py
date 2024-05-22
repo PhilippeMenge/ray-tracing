244990437dd8d023138f3297fa84ec7365d14e23
Walmir Bispo <wbsj@cin.ufpe.br>
2024-05-22 11:49:49 -0300

    Implementando outra forma de interseção de triangulos

--- src/triangulo.py
+++ src/triangulo.py
@@ -44,3 +44,41 @@ class Triangulo(Objeto):
             return distance
 
         return None
+
+    """
+    def get_intersecao(self, ray: Ray) -> None | float:
+        aresta_ab = self.points[1] - self.points[0] # edge1
+        aresta_ac = self.points[2] - self.points[0] # edge2
+        aresta_bc = self.points[2] - self.points[1] # edge3
+        
+        # Checando interseção com plano o qual o triangulo pertence
+        normal = aresta_ab.produto_vetorial(aresta_ac)
+        denominador = ray.direcao.produto_escalar(normal)
+        numerador = normal.produto_escalar(self.points[0] - ray.origem)
+        
+        # Ray é paralelo ao plano, sem interseção
+        if denominador == 0:
+            return None
+
+        t = numerador / denominador
+        if t < 0:
+            return None
+        
+        area_total = (aresta_ab.produto_vetorial(aresta_ac)).norma() / 2
+        ponto_intersecao = ray.origem + t*ray.direcao
+        
+        area_acp = (((ponto_intersecao - self.points[0]).produto_vetorial(aresta_ac)).norma())/2
+        u = area_acp/area_total
+        
+        area_abp = (((ponto_intersecao - self.points[0]).produto_vetorial(aresta_ab)).norma())/2
+        v = area_abp/area_total
+
+        area_bcp = (((ponto_intersecao - self.points[1]).produto_vetorial(aresta_bc)).norma())/2
+        w = area_bcp/area_total 
+        
+        EPSILON = 0.001
+        if 1-EPSILON <= (u + w + v) <= 1+EPSILON:
+            return t
+        
+        return None
+    """
