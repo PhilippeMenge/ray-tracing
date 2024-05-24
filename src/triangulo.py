5e9e3b827c37f090f448ae30c84d49cd58c0dc72
Gabriel Nobrega <gabrielnobrega63@gmail.com>
2024-05-24 15:54:01 -0300

    rearranjando código

--- src/triangulo.py
+++ src/triangulo.py
@@ -13,40 +13,6 @@ class Triangulo(Objeto):
         super().__init__(material)
         self.vertices = vertices
 
-    """
-   # Algoritmo de Möller–Trumbore
-   def get_intersecao(self, ray: Ray) -> None | float:
-       aresta_ab = self.arestas[1] - self.arestas[0]
-       aresta_ac = self.arestas[2] - self.arestas[0]
-
-       p = ray.direcao.produto_vetorial(aresta_ac)
-       det = aresta_ab.produto_escalar(p)
-
-       EPSILON = 0.0001
-       if -EPSILON < det < EPSILON:
-           return None
-
-       # Cacular o parametro u. Se u estiver fora do range [0, 1], não há interseção
-       inverso_det = 1 / det
-       vetor_T = ray.origem - self.arestas[0]
-       u = inverso_det * (vetor_T.produto_escalar(p))
-       if u < 0 or u > 1:
-           return None
-
-       # Cacular o parametro v. Se v estiver fora do range [0, 1], ou u+v>1, não há interseção
-       vetor_q = vetor_T.produto_vetorial(aresta_ab)
-       v = inverso_det * (ray.direcao.produto_escalar(vetor_q))
-       if v < 0 or u + v > 1:
-           return None
-
-       # Calcula a distancia da origem até o ponto de interseção
-       distance = inverso_det * (aresta_ac.produto_escalar(vetor_q))
-       if distance > EPSILON:
-           return distance
-
-       return None
-     """
-
     def get_intersecao(self, ray: Ray) -> None | float:
         aresta_ab = self.vertices[1] - self.vertices[0] # edge1
         aresta_ac = self.vertices[2] - self.vertices[0] # edge2
